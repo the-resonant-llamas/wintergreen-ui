c4321bfdc2b3d9e074802cfb30a9ab8355c22c1f
Kara Langlois <kibs086@gmail.com>
2017-06-16 21:55:58 -0400

    install react-materialize

--- src/app/rootContainer.js
+++ src/app/rootContainer.js
@@ -2,9 +2,15 @@
 
 import React from 'react';
 import { render } from 'react-dom';
+import { Button } from 'react-materialize';
 
 const RootContainer = function() {
-  return (<h1 className="red-text">Hello World</h1>);
+  return (
+    <div>
+      <h1 className="red-text">Hello World</h1>
+      <Button waves='light'>Button</Button>
+    </div>
+  );
 }
 
 module.exports = RootContainer;
